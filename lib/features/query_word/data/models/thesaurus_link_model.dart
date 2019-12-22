671bcce0f5b9b0c0e0ca0d8b36b633d0322e48c8
Shakleen <shakleenishfar@gmail.com>
2019-12-22 21:22:42 +0600

    ThesaurusLink entity created. Model extends entity

--- lib/features/query_word/data/models/thesaurus_link_model.dart
+++ lib/features/query_word/data/models/thesaurus_link_model.dart
@@ -1,12 +1,12 @@
 import 'package:equatable/equatable.dart';
+import 'package:vocab/features/query_word/domain/entities/thesaurus_link.dart';
 
 /// [ThesaurusLinkModel] class
 /// entry_id (string): identifier of a word ,
 /// sense_id (string): identifier of a sense
-class ThesaurusLinkModel extends Equatable {
-  final String entryId, senseId;
-
-  ThesaurusLinkModel({this.entryId, this.senseId});
+class ThesaurusLinkModel extends ThesaurusLink {
+  ThesaurusLinkModel({String entryId, String senseId})
+      : super(entryId: entryId, senseId: senseId);
 
   @override
   List<Object> get props => [this.entryId, this.senseId];
