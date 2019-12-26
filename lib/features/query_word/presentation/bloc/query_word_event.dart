1aade1072f463c901a279aa2ae3f07d78079da60
Shakleen <shakleenishfar@gmail.com>
2019-12-26 19:49:47 +0600

    Event created

--- lib/features/query_word/presentation/bloc/query_word_event.dart
+++ lib/features/query_word/presentation/bloc/query_word_event.dart
@@ -1,5 +1,16 @@
+import 'package:meta/meta.dart';
+
 import 'package:equatable/equatable.dart';
 
 abstract class QueryWordEvent extends Equatable {
   const QueryWordEvent();
+}
+
+class GetWordEntryEvent extends QueryWordEvent {
+  final String queryWord;
+
+  GetWordEntryEvent({@required this.queryWord});
+
+  @override
+  List<Object> get props => [queryWord];
 }
