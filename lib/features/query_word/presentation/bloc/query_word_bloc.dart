a4df9da8ab383c03b6679d1cc11f3aa6699e5588
Shakleen <shakleenishfar@gmail.com>
2019-12-26 23:23:50 +0600

    Refactored bloc code

--- lib/features/query_word/presentation/bloc/query_word_bloc.dart
+++ lib/features/query_word/presentation/bloc/query_word_bloc.dart
@@ -31,47 +31,42 @@ class QueryWordBloc extends Bloc<QueryWordEvent, QueryWordState> {
   Stream<QueryWordState> mapEventToState(QueryWordEvent event) async* {
     if (event is GetWordEntryEvent) {
       final inputEither = inputConverter.convertToLower(event.queryWord);
-      yield* inputEither.fold(
-        (InvalidInputFailure failure) async* {
-          yield Error(message: INVALID_INPUT_ERROR_MESSAGE);
-        },
-        (String queryWordLower) async* {
-          yield Loading();
-          final fetchEither = await retriever(
-            Param(queryWord: queryWordLower),
-          );
-          yield* fetchEither.fold(
-            (Failure failure) async* {
-              String errorMessage;
-              
-              switch(failure.runtimeType) {
-                case DeviceOfflineFailure:
-                  errorMessage = DEVICE_OFFLINE_ERROR_MESSAGE;
-                  break;
-                case NotFoundFailure:
-                  errorMessage = NOT_FOUND_ERROR_MESSAGE;
-                  break;
-                case InvalidFilterFailure:
-                  errorMessage = INVALID_FILTER_ERROR_MESSAGE;
-                  break;
-                case TooLongURLFailure:
-                  errorMessage = TOO_LONG_URL_ERROR_MESSAGE;
-                  break;
-                case ServerFailure:
-                  errorMessage = SERVER_FAILURE_ERROR_MESSAGE;
-                  break;
-                default:
-                  errorMessage = UNKNOWN_ERROR_MESSAGE;
-              }
+      yield* inputEither.fold(_invalidInputCase, _validInputCase);
+    }
+  }
 
-              yield Error(message: errorMessage);
-            },
-            (RetrieveEntry wordEntry) async* {
-              yield Loaded(retrieveEntry: wordEntry);
-            },
-          );
-        },
-      );
+  Stream<QueryWordState> _invalidInputCase(InvalidInputFailure failure) async* {
+    yield Error(message: INVALID_INPUT_ERROR_MESSAGE);
+  }
+
+  Stream<QueryWordState> _validInputCase(String queryWordLower) async* {
+    yield Loading();
+    final fetchEither = await retriever(Param(queryWord: queryWordLower));
+    yield* fetchEither.fold(_errorOccuredCase, _successfulFetchCase);
+  }
+
+  Stream<QueryWordState> _errorOccuredCase(Failure failure) async* {
+    yield Error(message: _getErrorMessage(failure));
+  }
+
+  Stream<QueryWordState> _successfulFetchCase(RetrieveEntry wordEntry) async* {
+    yield Loaded(retrieveEntry: wordEntry);
+  }
+
+  String _getErrorMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case DeviceOfflineFailure:
+        return DEVICE_OFFLINE_ERROR_MESSAGE;
+      case NotFoundFailure:
+        return NOT_FOUND_ERROR_MESSAGE;
+      case InvalidFilterFailure:
+        return INVALID_FILTER_ERROR_MESSAGE;
+      case TooLongURLFailure:
+        return TOO_LONG_URL_ERROR_MESSAGE;
+      case ServerFailure:
+        return SERVER_FAILURE_ERROR_MESSAGE;
+      default:
+        return UNKNOWN_ERROR_MESSAGE;
     }
   }
 }
