ec214e996f20f830b34f7868092b084d08bbb582
Shakleen <shakleenishfar@gmail.com>
2019-12-28 21:15:49 +0600

    Underlined title test

--- lib/features/query_word/presentation/widgets/title_text.dart
+++ lib/features/query_word/presentation/widgets/title_text.dart
@@ -7,9 +7,23 @@ class TitleText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(text,
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Theme.of(context).primaryColor,
+            style: BorderStyle.solid,
+            width: 1,
+          ),
+        ),
+      ),
+      child: Text(
+        text,
         style: Theme.of(context).textTheme.title.copyWith(
               color: Theme.of(context).primaryColor,
-            ));
+            ),
+      ),
+    );
   }
 }
