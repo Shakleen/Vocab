cda2d3cffa651b255e71cb8981bbb105d9d0f497
Shakleen <shakleenishfar@gmail.com>
2019-12-29 12:55:32 +0600

    WordCard entity created

---
+++ lib/features/word_card/domain/entities/word_card.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+import 'package:vocab/features/query_word/domain/entities/pronunciation.dart';
+import 'package:vocab/features/word_card/domain/entities/syllable.dart';
+import 'package:meta/meta.dart';
+
+import 'woord_card_details.dart';
+
+class WordCard extends Equatable {
+  final String word;
+  final Pronunciation pronunciation;
+  final Syllable syllables;
+  final List<WordCardDetails> detailList;
+
+  const WordCard({
+    @required this.word,
+    @required this.pronunciation,
+    @required this.syllables,
+    @required this.detailList,
+  });
+
+  @override
+  List<Object> get props => [
+        word,
+        pronunciation,
+        syllables,
+        detailList,
+      ];
+}
