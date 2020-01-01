4a543e4e53c4ffe03c1859ddb792ae8fa6ed145d
Shakleen <shakleenishfar@gmail.com>
2020-01-01 23:12:21 +0600

    Deleted unncessary planning docs

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:vocab/core/ui/pages/base_page.dart';
-import 'package:vocab/features/word_card/presentation/bloc/bloc.dart';
-import 'package:vocab/features/word_card/presentation/widgets/word_card_body.dart';
-
 import 'injection_container.dart' as di;
 
 void main() async {
