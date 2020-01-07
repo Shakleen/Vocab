286d709434ab40110972a0049710c0e849265f7f
Shakleen <shakleenishfar@gmail.com>
2020-01-07 11:30:28 +0600

    Editing functionality started.

--- lib/features/word_card_save/presentation/pages/show_word_info_page.dart
+++ lib/features/word_card_save/presentation/pages/show_word_info_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vocab/core/database/card_database.dart';
+import 'package:vocab/core/navigation/routes.dart';
 import 'package:vocab/core/ui/widgets/app_title.dart';
 import 'package:provider/provider.dart';
 import 'package:vocab/core/ui/widgets/word_info_widget.dart';
@@ -11,30 +12,40 @@ class ShowWordInfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: AppTitle(),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: FutureBuilder(
-        future: Provider.of<CardDatabase>(context).wordDao.getWordCard(word),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasData) {
-              return WordInfoWidget(wordCard: snapshot.data);
-            }
+    return FutureBuilder(
+      future: Provider.of<CardDatabase>(context).wordDao.getWordCard(word),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        Widget body = Container();
+        Function onEditTapped;
+        if (snapshot.connectionState == ConnectionState.active) {
+          body = Center(child: CircularProgressIndicator());
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            body = WordInfoWidget(wordCard: snapshot.data);
+            onEditTapped = () {
+              Navigator.pushNamed(
+                context,
+                '${Page.CardFormPage}',
+                arguments: snapshot.data,
+              );
+            };
           }
+        }
 
-          return Container();
-        },
-      ),
+        return Scaffold(
+          appBar: AppBar(
+            title: AppTitle(),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.edit),
+                disabledColor: Colors.grey[300],
+                onPressed: onEditTapped,
+              )
+            ],
+          ),
+          body: body,
+        );
+      },
     );
   }
 }
