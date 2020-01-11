9d5f3e4cf9b21c9dacfdad830ba2f9997de104fc
Shakleen <shakleenishfar@gmail.com>
2020-01-11 14:04:25 +0600

    Refactoring

---
+++ lib/core/ui/pages/show_word_info_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:vocab/core/database/card_database.dart';
+import 'package:vocab/core/navigation/routes.dart';
+import 'package:vocab/core/ui/widgets/app_title.dart';
+import 'package:provider/provider.dart';
+import 'package:vocab/features/word_save/presentation/widgets/word_card_details_tab.dart';
+
+class ShowWordInfoPage extends StatefulWidget {
+  final String word;
+
+  const ShowWordInfoPage({Key key, @required this.word}) : super(key: key);
+
+  @override
+  _ShowWordInfoPageState createState() => _ShowWordInfoPageState();
+}
+
+class _ShowWordInfoPageState extends State<ShowWordInfoPage>
+    with TickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(
+      vsync: this,
+      length: 2,
+      initialIndex: 0,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future:
+          Provider.of<CardDatabase>(context).wordDao.getWordCard(widget.word),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return Scaffold(
+          appBar: AppBar(
+            title: AppTitle(),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.edit),
+                disabledColor: Colors.grey[300],
+                onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    '${Page.CardFormPage}',
+                    arguments: {
+                      'wordCard': snapshot.data,
+                      'isEditing': true,
+                    },
+                  );
+                },
+              )
+            ],
+            bottom: TabBar(
+              controller: _controller,
+              tabs: <Widget>[
+                Tab(text: 'Word details'),
+                Tab(text: 'Quiz cards'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            controller: _controller,
+            children: <Widget>[
+              WordCardDetailsTab(word: widget.word),
+              Container(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/features/word_save/presentation/widgets/word_card_details_tab.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:vocab/core/database/card_database.dart';
+import 'package:provider/provider.dart';
+import 'package:vocab/core/ui/widgets/word_info_widget.dart';
+
+class WordCardDetailsTab extends StatelessWidget {
+  final String word;
+
+  const WordCardDetailsTab({Key key, @required this.word}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Provider.of<CardDatabase>(context).wordDao.getWordCard(word),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          return WordInfoWidget(wordCard: snapshot.data);
+        }
+
+        return Container();
+      },
+    );
+  }
+}
