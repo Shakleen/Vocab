62563b9bdd20898a9fee379cac7ee7145322c3a2
Shakleen <shakleenishfar@gmail.com>
2020-01-20 20:00:28 +0600

    REfactoring

--- lib/features/word_card/presentation/widgets/word_card_body.dart
+++ lib/features/word_card/presentation/widgets/word_card_body.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vocab/core/ui/widgets/empty_state_ui.dart';
 import 'package:vocab/core/ui/widgets/error_state_ui.dart';
 import 'package:vocab/core/ui/widgets/loading_state_ui.dart';
-import 'package:vocab/features/word_card/presentation/widgets/loaded_state_ui.dart';
+import 'package:vocab/core/ui/widgets/word_info_widget.dart';
 import 'package:vocab/features/word_card/presentation/bloc/bloc.dart';
 
 class WordCardBody extends StatelessWidget {
@@ -18,7 +18,7 @@ class WordCardBody extends StatelessWidget {
           } else if (state is LoadingWordCardState) {
             return LoadingStateUI();
           } else if (state is LoadedWordCardState) {
-            return LoadedStateUI(wordCard: state.wordCard);
+            return WordInfoWidget(wordCard: state.wordCard);
           } else if (state is ErrorWordCardState) {
             return ErrorStateUI(message: state.message);
           }
